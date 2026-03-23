06dc7eba577e6624db4368ae7745c7c1b3b6b6af
Alex Axthelm <aaxthelm@RMI.org>
2026-03-23 10:48:47 +0100

    Initial setup of `shared` lib and workspace

---
+++ target/debug/deps/shared-db815391858e0d84.d
@@ -0,0 +1,5 @@
+/Users/aaxthelm/Documents/AlexAxthelm/pollux-rust/target/debug/deps/shared-db815391858e0d84.d: shared/src/lib.rs
+
+/Users/aaxthelm/Documents/AlexAxthelm/pollux-rust/target/debug/deps/libshared-db815391858e0d84.rmeta: shared/src/lib.rs
+
+shared/src/lib.rs:
