a47bb8f747c183b70a6e58713d7d9b4cbea6f277
Alex Axthelm <aaxthelm@RMI.org>
2026-03-30 21:38:42 +0200

    Begin work on updating to demo from v 0.17

--- target/debug/deps/shared-db815391858e0d84.d
+++
@@ -1,5 +0,0 @@
-/Users/aaxthelm/Documents/AlexAxthelm/pollux-rust/target/debug/deps/shared-db815391858e0d84.d: shared/src/lib.rs
-
-/Users/aaxthelm/Documents/AlexAxthelm/pollux-rust/target/debug/deps/libshared-db815391858e0d84.rmeta: shared/src/lib.rs
-
-shared/src/lib.rs:
