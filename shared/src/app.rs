7df32afe335cb297973cad3d5d73a0305248ab04
Alex Axthelm <aaxthelm@RMI.org>
2026-03-30 21:48:33 +0200

    wip: use newer tutorial instructions

--- shared/src/app.rs
+++ shared/src/app.rs
@@ -1,2 +1,56 @@
+use crux_core::App;
+
 #[derive(Default)]
 pub struct Counter;
+
+impl App for Counter {
+    type Event = Event;
+    type Model = Model;
+    type ViewModel = ViewModel;
+    type Effect = Effect;
+
+    fn update(&self, event: Event, model: &mut Model) -> Command<Effect, Event> {
+        match event {
+            Event::Increment => model.count += 1,
+            Event::Decrement => model.count -= 1,
+            Event::Reset => model.count = 0,
+        }
+
+        render()
+    }
+
+    fn view(&self, model: &Model) -> ViewModel {
+        ViewModel {
+            count: format!("Count is: {}", model.count),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum Event {
+    Increment,
+    Decrement,
+    Reset,
+}
+
+#[derive(Default)]
+pub struct Model {
+    count: isize,
+}
+
+#[derive(Serialize, Deserialize, Clone, Default)]
+pub struct ViewModel {
+    pub count: String,
+}
+
+use crux_core::macros::effect;
+use crux_core::render::RenderOperation;
+
+#[effect(typegen)]
+#[derive(Debug)]
+pub enum Effect {
+    Render(RenderOperation),
+}
+
+
+
